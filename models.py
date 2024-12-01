c16eb695dc06ef65b2877274e02238623d68f1c8
LachlanVitale12 <lachlanvitale50@lism.catholic.edu.au>
2024-12-01 20:02:52 +1100

    Functions are all complete with updated ID

--- models.py
+++ models.py
@@ -21,24 +21,27 @@ def search_players(name):
     connection.close()
     return players
 
-
-
 def add_players(NAME:str, TEAM:str, POS:str, GP:int, MPG:float, FT:float, TWO_P:float, THREE_P:float, eFG:float, PPG:float, RPG:float, APG:float, SPG:float, BPG:float, TPG:float):
     connection = sqlite3.connect("NBA_Player_Statistics.db")
     cursor = connection.cursor()
-    cursor.execute(f'INSERT INTO NBA_Player_Statistics (NAME,TEAM,POS,GP,MPG,FT,TWO_P,THREE_P,eFG,PPG,RPG,APG,SPG,BPG,TPG) VALUES ("{NAME}", "{TEAM}", "{POS}", "{GP}", "{MPG}", "{FT}", "{TWO_P}", "{THREE_P}", "{eFG}", "{PPG}", "{RPG}", "{APG}", "{SPG}", "{BPG}", "{TPG}")')
+    
+    #Grabs next available ID
+    cursor.execute('SELECT MAX(ID) FROM NBA_Player_Statistics')
+    max_id = cursor.fetchone()[0]
+    next_id = (max_id + 1)
+
+    cursor.execute(f'INSERT INTO NBA_Player_Statistics (ID,NAME,TEAM,POS,GP,MPG,FT,TWO_P,THREE_P,eFG,PPG,RPG,APG,SPG,BPG,TPG) VALUES ("{next_id}", "{NAME}", "{TEAM}", "{POS}", "{GP}", "{MPG}", "{FT}", "{TWO_P}", "{THREE_P}", "{eFG}", "{PPG}", "{RPG}", "{APG}", "{SPG}", "{BPG}", "{TPG}")')
     cursor.execute(f'SELECT * FROM NBA_Player_Statistics')
     players = cursor.fetchall()
     connection.commit()
     connection.close()
     return players
 
-
 # finds the player based on inputed name and deletes
-def delete_players(name):
+def delete_players(id):
     connection = sqlite3.connect("NBA_Player_Statistics.db")
     cursor = connection.cursor()
-    cursor.execute(f'DELETE from NBA_Player_Statistics WHERE NAME = "{name.capitalize()}"')
+    cursor.execute(f'DELETE from NBA_Player_Statistics WHERE ID = "{id}"')
     cursor.execute(f'SELECT * FROM NBA_Player_Statistics')
     players = cursor.fetchall()
     connection.commit()
